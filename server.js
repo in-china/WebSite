89de1ab810dd2f0581f21d13a968a12b897a3c39
LoveDeeply <2511434157@qq.com>
2021-08-11 10:53:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var server = require("http");
 server.createServer(function(request,response){
         response.writeHead(200,{"Content-Type":"text/html"});
-        response.end("Hello,NodeJS");
+        response.end("Hello,yuandi qifei");
 }).listen(9000);
 console.log("server running at http://127.0.0.1:9000");
