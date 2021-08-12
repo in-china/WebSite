711e69a62052bfef226ef99e84d8e319f11ea734
LoveDeeply <2511434157@qq.com>
2021-08-12 08:20:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-
+var http = require('http');
 var express = require('express');
+var serverIndex = require('server-index')
+
 var app = express();
+app.usr(serverIndex(./public));
+app.use(express.static('./public'));
  
-app.use(express.static(__dirname + '/public'));
- 
-app.listen(8080,() => {
-  console.log(`Server running at http::/lidong`);
-});
+//app.listen(8080,() => {console.log(`Server running at http::/lidong`);});
+
+//http server
+var http_sever = http.createServer(app);
+http_server.listen(80,'0.0.0.0');
+
+
 
